2a19043400e47cf606785b2361525fb61e72f9f9
xcd0 <xcd0@xcd0.com>
2019-01-20 13:50:42 +0900

    testing. commit by script.

---
+++ xcd0.github.io/p.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+dirhugo=~/work/hugo
+dirpage=$dirhugo/xcd0.github.io
+echo "------------------------------"
+echo "cd $dirpage"
+echo "------------------------------"
+cd $dirpage
+hugo
+git add -A .
+git commit -m "testing. commit by script."
+git push origin master
+
+echo "------------------------------"
+echo "cd $dirhugo"
+echo "------------------------------"
+cd $dirhugo
+git add -A .
+git commit -m "testing. commit by script"
+git push origin master
+
